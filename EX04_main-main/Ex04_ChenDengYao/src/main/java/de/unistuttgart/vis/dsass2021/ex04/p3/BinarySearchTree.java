7548e72494bda65a4860e8ca081d65df8b1e03ad
Shunyi98 <83218341+Shunyi98@users.noreply.github.com>
2021-05-31 00:28:23 +0200

    Add files via upload

--- EX04_main-main/Ex04_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex04/p3/BinarySearchTree.java
+++ EX04_main-main/Ex04_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex04/p3/BinarySearchTree.java
@@ -2,6 +2,8 @@ package de.unistuttgart.vis.dsass2021.ex04.p3;
 
 import java.util.Iterator;
 import java.util.Stack;
+import java.util.Queue;
+
 
 public class BinarySearchTree<T extends Comparable<T>> implements
 		IBinarySearchTreeIterable<T> { 
@@ -10,13 +12,32 @@ public class BinarySearchTree<T extends Comparable<T>> implements
 
 	public BinarySearchTree() {
 		this.root = null;
-		
-
-		    	
 	}
 
-	
-		
+	@Override
+	public void insert(T t) {
+		this.root = this.insert(this.root, t);
+	}
+
+	private IBinaryTreeNode<T> insert(IBinaryTreeNode<T> node, T t) {
+		if (node == null) {
+			IBinaryTreeNode<T> newNode = new BinaryTreeNode<>();
+			newNode.setValue(t);
+			return newNode;
+		}
+		if (t.compareTo(node.getValue()) < 0) {
+			node.setLeftChild(this.insert(node.getLeftChild(), t));
+		} else if (t.compareTo(node.getValue()) > 0) {
+			node.setRightChild(this.insert(node.getRightChild(), t));
+		}
+		return node;
+	}
+
+	@Override
+	public IBinaryTreeNode<T> getRootNode() {
+		return this.root;
+	}
+
 	public void PreorderIterator(IBinaryTreeNode<T> n) {
         if (n == null) {
             return;
@@ -49,7 +70,7 @@ public class BinarySearchTree<T extends Comparable<T>> implements
 
             while (current != null) {
 
-                s.push(current);
+                st.push(current);
                 current = current.getLeftChild();
             }
 
@@ -62,7 +83,7 @@ public class BinarySearchTree<T extends Comparable<T>> implements
         }
     }
 	
-	public void PostorderIterator(IBinaryTreeNode<T> n) {
+	/*public void PostorderIterator(IBinaryTreeNode<T> n) {
 
         if (n == null) {
             return;
@@ -90,48 +111,56 @@ public class BinarySearchTree<T extends Comparable<T>> implements
                 }
             }
         }
-    }
+    }*/
 
-	
-	
-	@Override
-	public void insert(T t) {
-		this.root = this.insert(this.root, t);
-	}
-
-	private IBinaryTreeNode<T> insert(IBinaryTreeNode<T> node, T t) {
-		if (node == null) {
-			IBinaryTreeNode<T> newNode = new BinaryTreeNode<>();
-			newNode.setValue(t);
-			return newNode;
-		}
-		if (t.compareTo(node.getValue()) < 0) {
-			node.setLeftChild(this.insert(node.getLeftChild(), t));
-		} else if (t.compareTo(node.getValue()) > 0) {
-			node.setRightChild(this.insert(node.getRightChild(), t));
-		}
-		return node;
-	}
-
-	@Override
-	public IBinaryTreeNode<T> getRootNode() {
-		return this.root;
-	}
-
-	@Override
-	public Iterator<T> iterator(TreeTraversalType traversalType) {
-		switch (traversalType) {
-		case PREORDER:
-			return new PreorderIterator<T>(this);
-		case INORDER:
-		    return new InorderIterator<T>(this);
-		case POSTORDER:
-		    return new PostorderIterator<T>(this);
-		case LEVELORDER:
-		    return new LevelorderIterator<T>(this);
-		}
+	public void PostorderIterator(IBinaryTreeNode<T> n)  {         ////
+		Stack<IBinaryTreeNode<T>> s = new Stack<IBinaryTreeNode<T>>();
+		Stack<IBinaryTreeNode<T>> s2 = new Stack<IBinaryTreeNode<T>>();
 		
-		return null;
+		//s2.InitStack(20);
+		if (n == null) {
+			return;
+		}
+		IBinaryTreeNode<T> temp = null;
+		temp = n;
+		while (!s.isEmpty() || temp != null) {
+			while (temp != null) {
+				s2.push(temp); // 这行改变
+				s2.push(temp);
+				temp = temp.getRightChild(); // 这行改变
+			}
+			temp = s.pop();
+			temp = temp.getLeftChild();
+		}
+		while (!s2.isEmpty()) { // 把第二个栈全部出栈打印
+			temp = s2.pop();
+			System.out.println(temp.getValue());
+
+		}
 	}
+
+	public void LevelorderIterator(IBinaryTreeNode<T> n) {
+		Queue<IBinaryTreeNode<T>> queue = new Queue<IBinaryTreeNode<T>>();
+		IBinaryTreeNode<T> temp = null;
+		queue.front = queue.rear = 0;
+		if (n == null) {
+			
+			return;
+		}
+		queue.enterQueue(n);
+		while (queue.front != queue.rear) {
+			temp = queue.deleteQueue();
+			System.out.println(temp.getValue());
+			if (temp.getLeftChild() != null) {
+				queue.enterQueue(temp.getLeftChild());
+			}
+			if (temp.getRightChild() != null) {
+				queue.enterQueue(temp.getRightChild());
+			}
+
+		}
+	}
+	}
+
 
 }

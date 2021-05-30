0d16d2d37e83bbdc12b84e3599d1ef6a4899b958
ad027yycn <58372516+ad027yycn@users.noreply.github.com>
2021-05-30 23:02:29 +0200

    Add files via upload

---
+++ EX04_main-main/Ex04_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex04/p3/BinarySearchTree.java
@@ -0,0 +1,137 @@
+package de.unistuttgart.vis.dsass2021.ex04.p3;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class BinarySearchTree<T extends Comparable<T>> implements
+		IBinarySearchTreeIterable<T> { 
+
+	private volatile IBinaryTreeNode<T> root;
+
+	public BinarySearchTree() {
+		this.root = null;
+		
+
+		    	
+	}
+
+	
+		
+	public void PreorderIterator(IBinaryTreeNode<T> n) {
+        if (n == null) {
+            return;
+        }
+        Stack<IBinaryTreeNode<T>> st = new Stack<IBinaryTreeNode<T>>();
+        IBinaryTreeNode<T> current = n;
+        while (current != null || !st.isEmpty()) {
+
+            while (current != null) {
+                System.out.println(current.getValue());
+                st.push(current);
+                current = current.getLeftChild();
+            }
+
+            if (!st.empty()) {
+                current = st.pop();
+                current = current.getRightChild();
+            }
+        }
+    }
+	
+	public void InorderIterator(IBinaryTreeNode<T> n) {
+
+        if (n == null) {
+            return;
+        }
+        Stack<IBinaryTreeNode<T>> st = new Stack<IBinaryTreeNode<T>>();
+        IBinaryTreeNode<T> current = n;
+        while (current != null || !st.isEmpty()) {
+
+            while (current != null) {
+
+                s.push(current);
+                current = current.getLeftChild();
+            }
+
+            if (!st.empty()) {
+                current = st.pop();
+                System.out.println(current.getValue());
+                current = current.getRightChild();
+            }
+
+        }
+    }
+	
+	public void PostorderIterator(IBinaryTreeNode<T> n) {
+
+        if (n == null) {
+            return;
+        }
+        Stack<IBinaryTreeNode<T>> s = new Stack<IBinaryTreeNode<T>>();
+        IBinaryTreeNode<T> current = n;
+        while (current != null || !s.isEmpty()) {
+
+            while (current != null) {
+                current.isFirst = true;
+                s.push(current);
+                current = current.getLeftChild();
+            }
+
+            if (!s.empty()) {
+                current = s.pop();
+
+                if (current.isFirst) {
+                    current.isFirst = false;
+                    s.push(current);
+                    current = current.getRightChild();
+                } else {
+                    System.out.println(current.getValue());
+                    current = null;
+                }
+            }
+        }
+    }
+
+	
+	
+	@Override
+	public void insert(T t) {
+		this.root = this.insert(this.root, t);
+	}
+
+	private IBinaryTreeNode<T> insert(IBinaryTreeNode<T> node, T t) {
+		if (node == null) {
+			IBinaryTreeNode<T> newNode = new BinaryTreeNode<>();
+			newNode.setValue(t);
+			return newNode;
+		}
+		if (t.compareTo(node.getValue()) < 0) {
+			node.setLeftChild(this.insert(node.getLeftChild(), t));
+		} else if (t.compareTo(node.getValue()) > 0) {
+			node.setRightChild(this.insert(node.getRightChild(), t));
+		}
+		return node;
+	}
+
+	@Override
+	public IBinaryTreeNode<T> getRootNode() {
+		return this.root;
+	}
+
+	@Override
+	public Iterator<T> iterator(TreeTraversalType traversalType) {
+		switch (traversalType) {
+		case PREORDER:
+			return new PreorderIterator<T>(this);
+		case INORDER:
+		    return new InorderIterator<T>(this);
+		case POSTORDER:
+		    return new PostorderIterator<T>(this);
+		case LEVELORDER:
+		    return new LevelorderIterator<T>(this);
+		}
+		
+		return null;
+	}
+
+}
